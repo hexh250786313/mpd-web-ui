54929e4581e750b1be0a474190b363b0fbc8cfa8
hexh <250786313@qq.com>
2022-07-14 22:43:50 +0800

    wip: add neverthrow

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -2,6 +2,7 @@ import { useTestJotaiStore } from '@stores'
 import React from 'react'
 import style from './index.module.scss'
 import useSWR from 'swr'
+import { ResultAsync, Result } from 'neverthrow'
 
 export const Test: React.FC<any> = () => {
   const { data, set } = useTestJotaiStore()
@@ -13,6 +14,25 @@ export const Test: React.FC<any> = () => {
       }, 200)
     })
   })
+
+  // const promise = new Promise((res, rej) => {
+  // Math.random() > 0.5 ? res(1) : rej(2)
+  // })
+
+  // const test = ResultAsync.fromPromise(promise, (e) => e as Error)
+
+  const mainErrorFn = () => {
+    if (Math.random() > 0.5) {
+      console.log('OK')
+      return 'OK'
+    }
+    throw new Error('Failed')
+  }
+
+  // const test = Result.fromThrowable(mainErrorFn)
+  const test = Result.fromThrowable(mainErrorFn, (e) => e as Error)
+
+  console.log(test())
 
   console.log(result)
 
